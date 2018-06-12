cd31305da948aaa737763d9ceb5dedafb7e325eb
Matheus Henrique <matheushenrique.dev@gmail.com>
2018-06-12 17:53:45 -0300

    forms reativos

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,6 +4,7 @@ import { ShoppingCart } from '../restaurant-detail/shopping-cart/shopping-cart.m
 import { OrderService } from './order-item/order-service';
 import{ Order, OrderItem } from './order-model'
 import {Router} from '@angular/router'
+import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
 import 'rxjs/add/operator/map'
 
 @Component({
@@ -13,6 +14,10 @@ import 'rxjs/add/operator/map'
 export class OrderComponent implements OnInit {
 
   delivery: number = 8
+  orderForm: FormGroup
+
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numeroPattern = /^[0-9]*$/
 
   paymentOptions: RadioOption[] = [
     {label: 'Dinheiro', value:'MON'},
@@ -22,10 +27,37 @@ export class OrderComponent implements OnInit {
   
 
   
-  constructor(private orderService: OrderService, private router: Router) { }
+  constructor(private orderService: OrderService, private router: Router,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.fb.group({
+      name: this.fb.control("",[Validators.required, Validators.minLength(5)]),
+      email: this.fb.control("",[Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.fb.control("",[Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.fb.control("",[Validators.required, Validators.minLength(5)]),
+      number: this.fb.control("",[Validators.required, Validators.pattern(this.numeroPattern)]),
+      optionalAdress: this.fb.control(""),
+      paymentOption: this.fb.control("",[Validators.required])
+    }, {Validator: OrderComponent.equalsTo})
+}
+
+static equalsTo(group: AbstractControl): {[Key:string]: boolean}{
   
+
+  const email = group.get('email')
+  const emailConfirmation = group.get('emailConfirmation')
+  
+  if(!email || !emailConfirmation){
+   return undefined
+  }
+
+  if(email.value !== emailConfirmation.value){ 
+    
+    return {emailsIsNotMatch:true}
+  }
+  
+  return undefined
 }
 
 itensValue(): number{
@@ -47,6 +79,7 @@ decreaseQty(item: ShoppingCart){
 remove(item: ShoppingCart){
  
   this.orderService.remove(item)
+  console.log(this.orderForm.get('email'))
 }
 
 checkData(item: Order){
